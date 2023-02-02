b5dc6e8c8071d773c4117c21a62950e8174a2e93
MaksimBrezho <70651566+MaksimBrezho@users.noreply.github.com>
2023-02-03 02:54:55 +0300

    Update data-to-database.py

--- src/server/aggregator/data-to-database.py
+++ src/server/aggregator/data-to-database.py
@@ -1,10 +1,14 @@
 import datetime
 import os
-
+import json
 import requests
 
 path_len = len([name for name in os.listdir('data/') if os.path.isfile(os.path.join('data/', name))])
 for i in range(path_len):
     this_date = str(datetime.date(2022, 11, 20) + datetime.timedelta(days=i))
-    with open(f'data/offers-{this_date}.json', 'rb', encoding='utf-8') as file:
-        r = requests.post(url='http://localhost:8080/insert-new-data', files={f'data/offers-{this_date}.json': file})
+    with open(f'data/offers-{this_date}.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    response = requests.post(url='http://localhost:8080/insert-new-data', json=data)
+    print(i, this_date, response)
+    
